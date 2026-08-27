fbb7515c7c434ea164e82f68165eb6ec4af9a052
maryfp12 <mefonseca@uninorte.edu.co>
2026-08-27 14:40:51 +0000

    feat: se hizo un menú

---
+++ mostrarMenu.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class mostrarMenu{ 
+   static ArrayList<String[]> mostrarMenu = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in); 
+
+    public static void main(String[] args) {
+        int opcion;
+        do {
+            mostrarMenu();
+            opcion = leerEntero("seleccione una opción: ");
+            switch (opcion) {
+                case 1 -> registrarUnlector();
+                case 2 -> listarLector();
+                case 3 -> eliminarLector();
+                case 4 -> registrarPrestamo();
+                case 5 -> listarPrestamo();
+                case 6 -> salirdelMenu();
+                default -> System.out.println("Opción inválida. Intente de nuevo.");
+            }
+            System.out.println();
+        } while (opcion != 6);
+        sc.close();
+    }
+     static void mostrarMenu() {
+        System.out.println("===  Gestión de lectores ===");
+        System.out.println("1. Registrar nuevo lector");
+        System.out.println("2. Listar a un lector");
+        System.out.println("3. Eliminar a un lector ");
+        System.out.println("4. Registar un préstamo");
+        System.out.println("5. Listar un préstamo");
+        System.out.println("6. Salir");
+    }
+
+     static void registrarUnlector() {
+     }
+     static void listarLector(){
+
+     }
+     static void eliminarLector(){
+
+     }
+     static void registrarPrestamo() {
+
+     }
+     static void listarPrestamo(){
+
+     }
+     static void salirdelMenu() {
+
+     }
+
+    static int leerEntero(String msg) {
+        while (true) {
+            System.out.print(msg);
+            try {
+                return Integer.parseInt(sc.nextLine().trim());
+            } catch (Exception e) {
+                System.out.println("Ingrese un número entero válido.");
+            }
+        }
+    }
+}
+
